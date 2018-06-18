077fa225030ed7e4435e33fa7e438cb59b89d5c7
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2018-06-18 16:09:47 -0600

    WIP Sophie Muller

---
+++ src/hello_world.py
@@ -0,0 +1,35 @@
+print("hello world")
+
+a = 1
+b = 'this is a string'
+c = [1, 2, 3, 4]
+d = [5, 6, 7, 8]
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print (len(c))
+c.append(42)
+print(c)
+c.append('does this work?')
+print(c)
+
+print (e['name'])
+e ['number'] += 9000
+print (e['number'])
+
+for n in c:
+    for m in d:
+        print (n * m)
+
+def main();
+    print('hello wold!')
+    
+    #count_to_one_hundred()
+    value = multiply_two_numbers{6, 7}
+    print{value}
+
+
+
